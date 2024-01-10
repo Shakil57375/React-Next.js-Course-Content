dc9b9fa018de323af9c0332dd46a1e905ca993c3
Shakil57375 <shakil57375@gmail.com>
2024-01-10 10:48:29 +0600

    react props and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-const style1 = {
-  backgroundColor: "red",
-};
+import Avatar from "./components/Avatar";
+import Profile from "./components/Profile";
+
 export default function App() {
-  const style2 = {
-    backgroundColor: "green",
-  };
-
-  const color = "green";
-//   let style = "";
-
-//   if (color === "green") {
-//     style = style1;
-//   } else {
-//     style = style2;
-//   }
-  const today = new Date();
-  const formateDate = (date) => {
-    return Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
-  };
-  return (
-    // <ul style={style}>
-    <ul style={color === "green" ? style2 : style1}>
-      <p>today is {formateDate(today)}</p>
-      <li>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
-      <li>Work on the alcohol-fuelled engine</li>
-    </ul>
-  );
+    return (
+        <div>
+            <div>
+                <Profile>
+                    <Avatar age={39} />
+                </Profile>
+            </div>
+        </div>
+    );
 }
