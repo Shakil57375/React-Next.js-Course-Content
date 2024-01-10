f5eae23896edee03b25277aa63e88a408a1a73ab
Shakil57375 <shakil57375@gmail.com>
2024-01-10 07:45:02 +0600

    import and export component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Gallery from "./components/Gallery";
+import { Card, Profile as UserInfo } from "./components/Gallery"; // named import  / Custom Name of import
+import Gallery from "./components/Gallery"; // default import
 
 function App() {
   return (
     <div>
       <Gallery />
+      <UserInfo />
+      <Card />
     </div>
   );
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,17 @@
-function Profile() {
+export function Profile() { // named export
     return (
         <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
     );
 }
+export function Card() { // named export
+    return (
+        <div className="card">
+            <h1>This is a card</h1>
+        </div>
+    );
+}
 
-export default function Gallery() {
+export default function Gallery() { // default export
     return (
         <section>
             <h1>Amazing scientists</h1>
