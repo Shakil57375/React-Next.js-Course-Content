24429aef59a1210fe11447340b2ba757bbcbb95d
Shakil57375 <shakil57375@gmail.com>
2024-01-10 10:36:02 +0600

    Basics of JSX: React's Markup - Writing Markup with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 const style1 = {
-    backgroundColor: "red",
+  backgroundColor: "red",
 };
 export default function App() {
-    const style2 = {
-        backgroundColor: "green",
-    };
+  const style2 = {
+    backgroundColor: "green",
+  };
 
-    const color = "green";
-    let style = "";
+  const color = "green";
+//   let style = "";
 
-    if (color === "green") {
-        style = style1;
-    } else {
-        style = style2;
-    }
-
-    return (
-        <ul style={style}>
-            <li>Improve the videophone</li>
-            <li>Prepare aeronautics lectures</li>
-            <li>Work on the alcohol-fuelled engine</li>
-        </ul>
-    );
+//   if (color === "green") {
+//     style = style1;
+//   } else {
+//     style = style2;
+//   }
+  const today = new Date();
+  const formateDate = (date) => {
+    return Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+  };
+  return (
+    // <ul style={style}>
+    <ul style={color === "green" ? style2 : style1}>
+      <p>today is {formateDate(today)}</p>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
+  );
 }
