9907c3ab5e55d9859d070d1a4e58ef57ffb83978
Shakil57375 <shakil57375@gmail.com>
2024-01-11 07:44:24 +0600

    maping data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+import Gallery from "./components/Gallery";
 import PackingList from "./components/PackingList";
-
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
 export default function App() {
-    return (
-        <div>
-            <div>
-                <PackingList />
-            </div>
-        </div>
-    );
+  const chemists = people.filter((person) => person.profession === "chemist");
+  return (
+    <div>
+      <div>
+        {chemists.map((chemist) => (
+          <div key={chemist.id}>
+            <li>{chemist.name}</li>
+          </div>
+        ))}
+        <PackingList />
+        <Gallery />
+      </div>
+    </div>
+  );
 }
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,26 +1,31 @@
 function Item({ name, isPacked }) {
-    let itemContent = "";
+  /* let itemContent = "";
 
     if (isPacked) {
         itemContent = <span>{name + "✔"}</span>;
     } else {
         itemContent = <span>{name}</span>;
-    }
+    } */
 
-    return <li className="item">{itemContent}</li>;
+  return (
+    <div>
+      {/* <li className="item">{isPacked ? name + "✔" : name}</li> */}
+      {name} {isPacked && '✔'}
+    </div>
+  );
 }
 
 export default function PackingList() {
-    return (
-        <section>
-            <h1>Sally Rides Packing List</h1>
-            <ul>
-                <Item isPacked={true} name="Space suit" />
-                <Item isPacked={true} name="Helmet with a golden leaf" />
-                <Item isPacked={false} name="Photo of Tam" />
-            </ul>
-        </section>
-    );
+  return (
+    <section>
+      <h1>Sally Rides Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
 }
 
 // just for memory
