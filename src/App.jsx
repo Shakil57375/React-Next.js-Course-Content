6e61d203b92044a9b5fe0fc5c345dccd50ef2a73
Shakil57375 <shakil57375@gmail.com>
2024-01-16 10:44:52 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
-import ComplexObject from "./Components/ComplexObject";
-import Form from "./Components/Form";
-import MovingDot from "./Components/MovingDot";
-import NestedObject from "./Components/NestedObject";
+import Fillter from "./Components/Fillter";
+import List from "./Components/List";
 
 function App() {
   return (
     <>
-     <h1>hlw</h1>
+      {/* <List /> */}
+      <Fillter />
     </>
   );
 }
