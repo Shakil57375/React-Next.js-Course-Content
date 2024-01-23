cdce0b684642b1ddeec456ac565cf191824a4e9b
Shakil57375 <shakil57375@gmail.com>
2024-01-23 11:30:56 +0600

    form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import Mirror from "./components/Mirror";
 
 function App() {
-    return (
-        <div>
-            <Form />
-        </div>
-    );
+  //! don't do this don't change the props form the prenet component
+  //   const [color, setColor] = useState("red");
+  //! don't do this don't change the props form the prenet component
+  //   const handleChangeColor = () => {
+  //     setColor("blue");
+  //   };
+
+  // TODO you can just pass the inital value of a component from parent. can't chnage the value from the parent this will not work.
+  const color = "red";
+
+  return (
+    <div>
+      <Mirror messageColor={color} />
+      <br />
+      {/* //! don't do this don't change the props form the prenet component */}
+      {/* <button onClick={handleChangeColor}>
+                Change Color from Parent
+            </button> */}
+    </div>
+  );
 }
 
 export default App;
